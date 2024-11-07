b6505869f88a62a132c78677f640f51af4a87f0f
SollyMollyDolly <sollymollydolly@gmail.com>
2024-11-07 17:47:36 -0300

    typo'd

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo If compilation fails, make sure you have 'cc' installed or manually compile src/main.c
+echo If compilation fails, make sure you have 'gcc' installed or manually compile src/main.c
 
 cc -Wall -o src/RNG/makerandom src/RNG/makerandom.c 
 src/RNG/makerandom
